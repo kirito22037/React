e02fd82d4ccf47e3ced8ecbca97af93a9d74d544
kirito22037 <kirito22037@gmail.com>
2020-01-18 00:37:16 +0530

    counter app with two card element and a heading element

---
+++ rai_counter/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Header from './components/header';
+import Card from './components/card';
+
+function App() {
+  return (
+    <React.Fragment>
+    <Header/>
+    <div className="App" className="row row-cols-1 row-cols-md-2">
+            <Card status="card text-white bg-primary mb-3" heading="Challenger"/>
+            <Card status="card text-white bg-danger mb-3" heading="Champion"/>
+    </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
